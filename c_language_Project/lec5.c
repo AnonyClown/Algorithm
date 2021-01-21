d7c29dc01745dfd35bdc4f234a17b52e5b8420bf
AnonyClown <skyt1258@gmail.com>
2021-01-21 21:24:49 +0900

    finished lec5.c

--- c_language_Project/lec5.c
+++ c_language_Project/lec5.c
@@ -1,16 +1,40 @@
 #include<stdio.h>
+#define MAX_SIZE 8
+int sorted[MAX_SIZE];
+
+int stack[MAX_SIZE];
+int top=-1; 
 
 void select_sort();
 void bubble_sort();
 void insert_sort();
 void rank();
+void divide_search();
+
+void mergesort(int list[],int left, int right);
+void merge(int list[], int left, int mid, int right);
+
+void Stack();
+int push(int num);
+int pop();
 
 int main()
 {
 	//select_sort();
 	//bubble_sort();
 	//insert_sort();
-	rank();
+	//rank();
+	//divide_search();
+
+	//int i;
+	//int list[8] = {1, 2, 3 ,4, 5, 6, 7, 8};
+	//mergesort(list, 0, 7);
+	
+	/*for(i=0; i<8; i++)
+	{
+		printf("%d ", list[i]);
+	}*/
+	Stack();
 }
 
 
@@ -149,14 +173,195 @@ void rank()
 	}
 }
 
+//이분 검색 
+void divide_search()
+{
+	int num[10]={1,2,3,4,5,6,7,8,9,10}, i,j,h,l,m;
+	
+	scanf("%d",&j);
+	
+	l=0;
+	h=9;
+	
+	while(1)
+	{
+		if(l<=h)
+		{
+			m=(l+h)/2;
+			
+			if(j==num[m])
+			{
+				printf("%d %d",j,m+1);
+				break;
+			}
+			
+			if(j<num[m])
+			{
+				h=m-1;
+			}
+			else
+			
+			{
+				l=m+1;
+			}
+		}
+		
+		else
+		{
+			printf("%d not found",j);
+			break;
+		}
+	}
+	
+}
 
+//merge sort : 리스트를 나누는 함수 
+void mergesort(int list[], int left, int right)
+{
+	int mid,i=0,j=0,k=0;
+	
+	if(left<right)
+	{
+		mid = (left+right)/2;
+			
+		mergesort(list, left,mid);
 
+			
+		mergesort(list, mid+1, right);
+	
+		
+		merge(list,left,mid,right);
+	
+	}
+	 
+}
+//merge sort: 리스트를 다시 합치는 함수 
+void merge(int list[], int left, int mid, int right)
+{
+	int i,j,k,l;
+	i=left;
+	j=mid+1;
+	k=left;
+	
+	while(i<=mid && j<=right) // list= [1234 5678]
+	{
+		if(list[i]<=list[j])
+		{
+			sorted[k++] = list[i++]; //sorted[0~3]까지 값 입력 
+		}
+		else
+		{
+			sorted[k++] = list[j++];
+		}
+	}
 
+	if(i>mid)
+	{
+		for(l=j; l<=right; l++)
+		{
+			sorted[k++] = list[l];
+		}
+	}
+	else
+	{
+    	for(l=i; l<=mid; l++)
+      	{
+      		sorted[k++] = list[l];
+		}
+  	}
 
+  	// 배열 sorted[](임시 배열)의 리스트를 배열 list[]로 재복사
+  	for(l=left; l<=right; l++)
+	{
+  	  list[l] = sorted[l];
+	}
 
+}
 
+//스택이란 한 쪽 끝에서만 자료를 넣고 뺄 수 있는 LIFO 형식의 자료 구조 
+// 가장 최근에 스택에 추가한 항목이 가장 먼저 제거될 항목
+//  
+void Stack()
+{
+	int l; 
+	while(1)
+	{
+		int i,j,k;
+		printf("push(1) or pop(2) : ");
+		scanf("%d", &i);
+		
+		if(i==1)
+		{
+			printf("삽입할 숫자를 작성하시오 : ");
+			scanf("%d",&j);
+			k=push(j);
+			if(k==-1)
+			{
+				break;
+			}
+			
+		}
+		else if(i==2)
+		{
+			k=pop();
+			if(k==-1)
+			{
+				break;
+			}
+			else
+			{
+				printf("제거한 자료는 %d 입니다.\n",k);
+			}
+		}
+		else
+		{
+			break;
+		}
+	 }
+	 
+	 if(top<0)
+	 {
+	 	printf("스택에 자료가 없습니다. ");
+	 }
+	 else
+	 {
+	 	printf("스택에 자료는 다음과 같습니다. \n");
+	 	for(l=top; l>=0; l--)
+	 	{
+	 		printf("%d ", stack[l]);
+		 }
+	}
+	 
+	 	  
+}
 
+int push(int num)
+{
+	top++;
+	if(top>MAX_SIZE)
+	{
+		printf("overflow");
+		top--;
+		return -1;
+	}
+	
+	stack[top] = num;
+	return 0;
+}
 
+int pop()
+{
+	int r;
+	if(top<0)
+	{
+		printf("underflow");
+		return -1;
+	}
+	
+	r=stack[top];
+	top--;
+	return r;
+}
 
 
 
