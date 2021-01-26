87bb8889a033646ad88dd8df00c530e577b21332
AnonyClown <skyt1258@gmail.com>
2021-01-26 16:45:41 +0900

    finished lec6

--- c_language_Project/lec6.c
+++ c_language_Project/lec6.c
@@ -5,6 +5,10 @@ void riul();
 void diamond();
 void hourglass();
 void righttriangle();
+void lsosceles_triangle();
+void degree_rotation();
+void snail();
+void diagonal();
 
 int main()
 {
@@ -13,7 +17,11 @@ int main()
 	//riul();
 	//diamond();
 	//hourglass();
-	righttriangle();
+	//righttriangle();
+	//lsosceles_triangle();
+	//degree_rotation();
+	//snail();
+	diagonal();
 
 }
 
@@ -183,7 +191,7 @@ void righttriangle()
 	int num[7][7]={0, };
 	int i,j,a,b;
 	int e,cnt=1,tmp=0;
-	
+
 	e=6;
 	
 	for(i=0; i<7; i++)
@@ -216,20 +224,174 @@ void righttriangle()
 }
 
 
+void lsosceles_triangle()
+{
+	int col, row; //가로 세로 배열처럼 표현 
+	int i, j, y, z; //for문 조건 변수들
+	int k, L, e; // l : 처음 행의 위치, e : 마지막 행의 위치 
+	
+	scanf("%d", &col);
+	row=col*2-1;
+	int a[row][col];
+	
+	for(i=0; i<row; i++)
+	{
+		for(j=0;j<col; j++)
+		{
+			a[i][j]=0;
+		}
+	}
+		
+	k=0;
+	for(j=0; j<=col-1; j++) //j는 열의 순서대로 진행함. 
+	{
+		L=col-(j+1);
+		e=(j-1) +col;
+		
+		for(i=L; i<=e; i++)
+		{
+			k++;
+			a[i][j]=k;
+		}
+	}
+	
+	for(y=0; y<row; y++)
+	{
+		for(z=0; z<col; z++)
+		{
+			printf("%3d",a[y][z]);
+		}
+		printf("\n");
+	}
+	
+}
 
+void degree_rotation()
+{
+	int i,j,k,x,y;
+	int a[5][5] = {0,}, b[5][5]={0,};
+	
+	k=0;
+	for(i=4; i>=0; i--)
+	{
+		for(j=i; j<=4; j++)
+		{
+			k++;
+			a[i][j]=k;
+		}
+	}
+	
+	for(i=0; i<=4; i++)
+	{
+		for(j=0; j<=4; j++)
+		{
+			b[j][4-i] = a[i][j];
+		}
+	}
+	
+	for(x=0; x<5; x++)
+	{
+		for(y=0 ;y<5; y++)
+		{
+			printf("%3d",a[x][y]);
+		}
+		printf("\n");
+	}
+	printf("\n\n");
+	for(x=0; x<5; x++)
+	{
+		for(y=0 ;y<5; y++)
+		{
+			printf("%3d",b[x][y]);
+		}
+		printf("\n");
+	}
+	
+	
+}
 
+void snail()
+{
+	int k,c,i,j,f,n,x,y;
+	int a[5][5]= {0};
+	
+	k=0;
+	c=1;
+	i=0;
+	j=-1;
+	
+	f=5;
+	while(1)
+	{
+		for(n=1; n<=f; n++)
+		{
+			k++;
+			j+=c;
+			a[i][j]=k;
+		}
+		
+		f--;
+		if(f<=0)
+		{
+			break;
+		}
+		
+		for(n=1; n<=f; n++)
+		{
+			k++;
+			i+=c;
+			a[i][j]=k;
+		}
+		c*=-1;
+	}
+	for(x=0; x<5; x++)
+	{
+		for(y=0; y<5; y++)
+		{
+			printf("%3d",a[x][y]);
+		}
+		printf("\n");
+	}
+	
+	
 
+}
 
-
-
-
-
-
-
-
-
-
-
+void diagonal()
+{
+	int a[5][5]={0};
+	int i,j,k,x,y;
+	int L=0;
+	
+	for(i=0; i<=8; i++)
+	{
+		for(j=0; j<=4; j++)
+		{
+			k=i-j;
+			if(k<0)
+			{
+				continue;
+			}
+			if(k>4)
+			{
+				continue;
+			}
+			
+			L++;
+			a[j][k]=L;
+		}
+	}
+	
+	for(x=0; x<5; x++)
+	{
+		for(y=0; y<5; y++)
+		{
+			printf("%3d",a[x][y]);
+		}
+		printf("\n");
+	}
+	
+}
 
 
 
